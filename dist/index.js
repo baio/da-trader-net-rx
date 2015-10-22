29bd49243dccae34a49dc96cb92abcc243989126
baio <max.putilov@gmail.com>
2015-10-22 23:36:08 +0300

    fix types

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,19 @@
 var traderNet = require("./trader-net");
 var utils = require("./utils");
+var ticketCodes = require("./enums/ticket-codes");
+var currencyCodes = require("./enums/currency-codes");
+var orderCodes = require("./enums/order-codes");
+var securityTypes = require("./enums/security-types");
 var traderNetRx;
 (function (traderNetRx) {
+    traderNetRx.TicketCodes = ticketCodes.TicketCodes;
+    traderNetRx.CurrencyCodes = currencyCodes.CurrencyCodes;
+    traderNetRx.OrderTypes = orderCodes.OrderTypes;
+    traderNetRx.OrderTypes = orderCodes.OrderTypes;
+    traderNetRx.OrderStatusCodes = orderCodes.OrderStatusCodes;
+    traderNetRx.OrderActionTypes = orderCodes.OrderActionTypes;
+    traderNetRx.SecurityType = securityTypes.SecurityType;
+    traderNetRx.SecurityKind = securityTypes.SecurityKind;
     traderNetRx.getSecurity = utils.getSecurity;
     traderNetRx.TraderNet = traderNet.TraderNet;
 })(traderNetRx || (traderNetRx = {}));
--- src/index.ts
+++ src/index.ts
@@ -8,13 +8,21 @@ import securityTypes = require("./enums/security-types");
 
 module traderNetRx {
 	export type TicketCodes = ticketCodes.TicketCodes;
+	export var TicketCodes = ticketCodes.TicketCodes;
 	export type CurrencyCodes = currencyCodes.CurrencyCodes;
+	export var CurrencyCodes = currencyCodes.CurrencyCodes;
 	export type OrderTypes = orderCodes.OrderTypes;
+	export var OrderTypes = orderCodes.OrderTypes;
 	export type OrderExpirationTypes = orderCodes.OrderExpirationTypes;
+	export var OrderTypes = orderCodes.OrderTypes;
 	export type OrderStatusCodes = orderCodes.OrderStatusCodes;
+	export var OrderStatusCodes = orderCodes.OrderStatusCodes;
 	export type OrderActionTypes = orderCodes.OrderActionTypes;
+	export var OrderActionTypes = orderCodes.OrderActionTypes;
 	export type SecurityType = securityTypes.SecurityType;
+	export var SecurityType = securityTypes.SecurityType;
 	export type SecurityKind = securityTypes.SecurityKind;
+	export var SecurityKind = securityTypes.SecurityKind;
 	export var getSecurity = utils.getSecurity;
 	export type TraderNet = traderNet.TraderNet;
 	export var TraderNet = traderNet.TraderNet;
